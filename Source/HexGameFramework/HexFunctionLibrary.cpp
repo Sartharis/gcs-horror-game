7a5dcfa8c5f17cb936d52ca00b0d4c9da9efc29c
Sartharis <jan.orlowsk@gmail.com>
2017-12-03 18:13:39 -0500

    Added Resolution functions

--- Source/HexGameFramework/HexFunctionLibrary.cpp
+++ Source/HexGameFramework/HexFunctionLibrary.cpp
@@ -138,3 +138,25 @@ AHexGameFrameworkGameModeBase* UHexFunctionLibrary::GetHexGameMode( const UObjec
 	return Cast<AHexGameFrameworkGameModeBase>( UGameplayStatics::GetGameMode( WorldContextObject ) );
 }
 
+FVector2D UHexFunctionLibrary::GetGameViewportSize()
+{
+	FVector2D Result = FVector2D( 1, 1 );
+
+	if( GEngine && GEngine->GameViewport )
+	{
+		GEngine->GameViewport->GetViewportSize( /*out*/Result );
+	}
+
+	return Result;
+}
+
+FVector2D UHexFunctionLibrary::GetGameResolution()
+{
+	FVector2D Result = FVector2D( 1, 1 );
+
+	Result.X = GSystemResolution.ResX;
+	Result.Y = GSystemResolution.ResY;
+
+	return Result;
+}
+
